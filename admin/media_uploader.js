f930b7ca8f67eafc4ebc8c640e97cc11091f690b
AsifMahsud <55624875+AsifMahsud@users.noreply.github.com>
2021-09-23 21:13:12 +0500

    Add files via upload

---
+++ admin/media_uploader.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+  var mediaUploader;
+  $('#upload_image_button').click(function(e) {
+    e.preventDefault();
+      if (mediaUploader) {
+      mediaUploader.open();
+      return;
+    }
+    mediaUploader = wp.media.frames.file_frame = wp.media({
+      title: 'Choose Image',
+      button: {
+      text: 'Choose Image'
+    }, multiple: false });
+    mediaUploader.on('select', function() {
+      var attachment = mediaUploader.state().get('selection').first().toJSON();
+      $('#clp_plugin_setting_logo').val(attachment.url);
+    });
+    mediaUploader.open();
+  });
+});
